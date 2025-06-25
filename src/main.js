b3e71fea00c3a6202394a1785e4c912c7d264e75
Hayrettin Ekizoglu <hayrettinekizoglu2@gmail.com>
2025-06-25 14:30:40 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,69 @@
+import * as THREE from "three";
+import { Renderer } from "./components/Renderer";
+import { Camera } from "./components/Camera";
+import { DirectionalLight } from "./components/DirectionalLight";
+import { player, initializePlayer } from "./components/Player";
+import { map, initializeMap } from "./components/Map";
+import { animateVehicles } from "./animateVehicles";
+import { animatePlayer } from "./animatePlayer";
+import { hitTest } from "./hitTest";
+import "./style.css";
+import "./collectUserInput";
+
+const scene = new THREE.Scene();
+scene.add(player);
+scene.add(map);
+
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
+
+const dirLight = DirectionalLight();
+dirLight.target = player;
+player.add(dirLight);
+
+const camera = Camera();
+player.add(camera);
+
+const scoreDOM = document.getElementById("score");
+const resultDOM = document.getElementById("result-container");
+
+initializeGame();
+
+document
+  .querySelector("#retry")
+  ?.addEventListener("click", initializeGame);
+
+function initializeGame() {
+  initializePlayer();
+  initializeMap();
+
+  // Initialize UI
+  if (scoreDOM) scoreDOM.innerText = "0";
+  if (resultDOM) resultDOM.style.visibility = "hidden";
+}
+
+const renderer = Renderer();
+renderer.setAnimationLoop(animate);
+
+function animate() {
+  animateVehicles();
+  animatePlayer();
+  hitTest();
+
+  renderer.render(scene, camera);
+}
+function onGameOver() {
+  showGameOverUI(); // Skor tablosunu göster
+
+  // Oyuncunun pozisyonunu doğrudan değiştir
+  player.position.set(0, 0.5, 0);  // player.mesh değil! sadece player
+  player.rotation.set(0, 0, 0);
+
+  // (isteğe bağlı) hareketi durdurmak için flag
+  player.userData.canMove = false;
+}
+
+
+function showGameOverUI() {
+  document.getElementById('gameOverScreen').classList.remove('hidden');
+}
