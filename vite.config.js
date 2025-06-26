d9bbc70c91570ed3200420b756dee71e8636da29
Hayrettin Ekizoglu <hayrettinekizoglu2@gmail.com>
2025-06-26 13:25:07 +0300

    css v1

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 // vite.config.js
 export default {
+  base: '/my-crossy-road-game/', // repo adınla birebir aynı
   build: {
     rollupOptions: {
       external: ['rollup', 'node:path', 'fsevents']
